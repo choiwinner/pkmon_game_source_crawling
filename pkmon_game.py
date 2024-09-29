ccb3305e8263c7d0aab9825ab7841117134c7d93
choiwinner <babyrain@naver.com>
2024-09-30 00:04:24 +0900

    feat : 크롤링 소스 코드 완성_240930 -. 포켓몬 이름 크롤링(완성) -. 포켓몬 이미지 크롤링(기초만 완성)

--- pkmon_game.py
+++ pkmon_game.py
@@ -1,3 +1,61 @@
 # 포켓몬 게임 개발 시작(240929)
 # source code
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
+import requests
+import re
+import dload
+import pandas as pd
+
+dload.save('https://data1.pokemonkorea.co.kr/newdata/pokedex/full/000101.png','1.png')
+
+def korean(x):
+    korean_pattern = re.compile('[가-힣]+')
+    text = korean_pattern.findall(x)
+    return text
+
+texts = []
+
+for i in range(1,500):
+    html = requests.get(f'https://pokemonkorea.co.kr/pokedex/view/{i}') #1~1252
+    # 2. 데이터 받아오기
+    if html.status_code == 200: #코드 응답이 정상이면 실행
+        soup = bs(html.text, 'html.parser') # 2. bs html.parser의 text parser로 설정
+        text = soup.find('h3').getText()
+        texts.append(text)
+
+for i in range(500,1001):
+    html = requests.get(f'https://pokemonkorea.co.kr/pokedex/view/{i}') #1~1252
+    # 2. 데이터 받아오기
+    if html.status_code == 200: #코드 응답이 정상이면 실행
+        soup = bs(html.text, 'html.parser') # 2. bs html.parser의 text parser로 설정
+        text = soup.find('h3').getText()
+        texts.append(text)
+
+for i in range(1001,1253):
+    html = requests.get(f'https://pokemonkorea.co.kr/pokedex/view/{i}') #1~1252
+    # 2. 데이터 받아오기
+    if html.status_code == 200: #코드 응답이 정상이면 실행
+        soup = bs(html.text, 'html.parser') # 2. bs html.parser의 text parser로 설정
+        text = soup.find('h3').getText()
+        texts.append(text)
+
+df = pd.DataFrame(texts,columns=['Name'])
+df.to_csv('포켓몬이름.csv',encoding='utf-8',index=False)
+
+html = requests.get('https://pokemonkorea.co.kr/pokedex/view/1')
+if html.status_code == 200: #코드 응답이 정상이면 실행
+    soup = bs(html.text, 'html.parser') # 2. bs html.parser의 text parser로 설정
+    img = soup.find('div', attrs={"class":"col-lg-6 col-12"}).img["src"]
+    dload.save(img,'1.png')
+
+
+## 1. 크롤링하는 site url을 requests.get('url')으로 설정
+#html = requests.get('https://pokemonkorea.co.kr/pokedex')
+#
+## 2. 데이터 받아오기
+#if html.status_code == 200: #코드 응답이 정상이면 실행
+#    # 2. bs html.parser의 text parser로 설정
+#    soup = bs(html.text, 'html.parser')
+#
+#    # 3. 미세먼지 정보가 있는 div 요소 추출
+#    dayconfirm = soup.find('span',"data1")
